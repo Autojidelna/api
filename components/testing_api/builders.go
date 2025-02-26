4f5e0d7805688ef1846d7ad605aea8c5a566a448
Pavel Dobiáš <git@paveldobias.eu>
2025-02-27 00:00:08 +0100

    basic dynamic daily meals generation done

--- components/testing_api/builders.go
+++ components/testing_api/builders.go
@@ -7,6 +7,9 @@ import (
 	"time"
 
 	"encoding/json"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 type LunchMeal struct {
@@ -14,11 +17,18 @@ type LunchMeal struct {
 }
 
 type Meal struct {
-	Nazev    string `json:"nazev"`
-	Varianta string `json:"varianta"`
+	Nazev       string     `json:"nazev"`
+	Varianta    string     `json:"varianta"`
+	Objednano   bool       `json:"objednano"`
+	LzeObjednat bool       `json:"lzeObjednat"`
+	NaBurze     bool       `json:"naBurze"`
+	Cena        float32    `json:"cena"`
+	Alergeny    []Allergen `json:"alergeny"`
 }
 
 type Allergen struct {
+	Nazev string `json:"nazev"`
+	Popis string `json:"popis"`
 }
 
 func buildLunches(date time.Time) template.HTML {
@@ -56,94 +66,115 @@ func buildLunches(date time.Time) template.HTML {
 	dayMeals := lunches[lunchIdString]
 	fmt.Println("Jidla")
 	lunchString = dayMeals.Jidla[0].Nazev
+	lunchString = buildMeals(date, dayMeals.Jidla)
 
 	return template.HTML(lunchString)
 }
 
-func buildMeals(date time.Time, meals []LunchMeal) string {
+func buildMeals(date time.Time, meals []Meal) string {
+	// Begin Main Wrapper
 	mealsString := fmt.Sprintf(
-		`<div id="orderContent%s" class="orderContent">
-    <div class="jidelnicekMain">
-        <div class="jidelnicekItem " role="group">
-            <div class="jidelnicekItemWrapper">
-                <div class="jidWrapLeft">
-                    <a href="#" class="btn button-link button-link-main maxbutton ordered"
-                        onClick="ajaxOrder(this, 'db/dbProcessOrder.jsp?time=1737310888909&amp;token=;ID=755744&amp;day=2025-01-21&amp;type=delete&amp;week=&amp;terminal=false&amp;keyboard=false&amp;printer=false', '2025-01-21', 'ordered');"
-                        role="button">
-                        <span class="button-link-align">zrušit<span class="warning"
-                                style="color:black">&nbsp;1&nbsp;ks</span></span>
-                        <span class="smallBoldTitle button-link-align">Oběd 1</span><span
-                            class="button-link-align">za</span>
-                        <span class="important warning button-link-align"
-                            title="Cena objednaného jídla">42,00&nbsp;Kč</span>
-                        <span class="button-link-tick">
-                            <i class="fa fa-check fa-2x"
-                                title="Máte objednáno<b>&nbsp;1&nbsp;ks</b> á <b>42,00&nbsp;Kč</b>"></i>
-                        </span>
-                    </a>
-                </div>
-                <div class="jidWrapCenter" id="menu-3-day-2025-01-21">
-                    Polévka hovězí s játrovou rýží * , hovězí maso námořnické *směs , přidává se
-                    víno a vejce , rýže dušená * , čaj * , sirup , voda , salátový bar <span
-                        class="textGrey">(<span
-                            title="Obiloviny obsahující lepek - nejedná se o celiakii, výrobky z nich"
-                            class="textGrey">Obiloviny</span><span>, </span><span
-                            title="Vejce a výrobky z nich - patří mezi potraviny ohrožující život"
-                            class="textGrey">Vejce</span><span>, </span><span
-                            title="Mléko  a  výrobky  z  něj - patří mezi potraviny ohrožující život"
-                            class="textGrey">Mléko</span><span>, </span><span
-                            title="Skořápkové  plody a výrobky z nich - jedná se o všechny druhy ořechů"
-                            class="textGrey">Skořápkové plody</span><span>, </span><span
-                            title="Celer a výrobky z  něj" class="textGrey">Celer</span><span>,
-                        </span><span title="Hořčice a  výrobky z ní"
-                            class="textGrey">Hořčice</span>)</span>
-                    <br>
-                </div>
-                <div id="icons28428" class="icons jidWrapRight">
-                    <i class="far fa-clock fa-2x inlineIcon"
-                        title="výdej&nbsp;od:&nbsp;<b>11:00:00</b>&nbsp;do:&nbsp;<b>14:40:00</b><br/> objednat&nbsp;do:&nbsp;<b>20.01.2025 15:00:00</b><br/> zrušit&nbsp;do:&nbsp;<b>20.01.2025 15:00:00</b>"></i>
-                </div>
-            </div>
-        </div>
-        <div class="jidelnicekItem " role="group">
-            <div class="jidelnicekItemWrapper">
-                <div class="jidWrapLeft">
-                    <a href="#" class="btn button-link button-link-main maxbutton enabled"
-                        onClick="ajaxOrder(this, 'db/dbProcessOrder.jsp?time=1737310888909&amp;token=;ID=5&amp;day=2025-01-21&amp;type=reorder&amp;week=&amp;terminal=false&amp;keyboard=false&amp;printer=false', '2025-01-21', 'enabled');"
-                        role="button">
-                        <span class="button-link-align">přeobjednat</span>
-                        <span class="smallBoldTitle button-link-align">Oběd 2</span><span
-                            class="button-link-align">s doplatkem</span>
-                        <span class="important warning button-link-align"
-                            title="Rozdíl ceny oproti stávající objednávce&nbsp;Cena při objednání jídla:&nbsp;42,00&nbsp;Kč">0,00&nbsp;Kč</span>
-                    </a>
-                </div>
-                <div class="jidWrapCenter" id="menu-4-day-2025-01-21">
-                    Polévka hovězí s játrovou rýží * , kuře na způsob bažanta * , brambory
-                    šťouchané , čaj * , sirup , voda , salátový bar , pro celiaky-polévka s rýží
-                    a masem <span class="textGrey">(<span
-                            title="Obiloviny obsahující lepek–nejedná se o celiakii, výrobky z nich"
-                            class="textGrey">Obiloviny</span><span>, </span><span
-                            title="Vejce a výrobky z nich - patří mezi potraviny ohrožující život"
-                            class="textGrey">Vejce</span><span>, </span><span
-                            title="Mléko  a  výrobky  z  něj - patří mezi potraviny ohrožující život"
-                            class="textGrey">Mléko</span><span>, </span><span
-                            title="Skořápkové  plody a výrobky z nich – jedná se o všechny druhy ořechů"
-                            class="textGrey">Skořápkové plody</span><span>, </span><span
-                            title="Celer a výrobky z  něj" class="textGrey">Celer</span><span>,
-                        </span><span title="Hořčice a  výrobky z ní"
-                            class="textGrey">Hořčice</span>)</span>
-                    <br>
-                </div>
-                <div id="icons28429" class="icons jidWrapRight">
-                    <i class="far fa-clock fa-2x inlineIcon"
-                        title="výdej&nbsp;od:&nbsp;<b>11:00:00</b>&nbsp;do:&nbsp;<b>14:40:00</b><br/> objednat&nbsp;do:&nbsp;<b>20.01.2025 15:00:00</b><br/> zrušit&nbsp;do:&nbsp;<b>20.01.2025 15:00:00</b>"></i>
-                </div>
-            </div>
-        </div>`, date.Format("2006-01-02"))
-	mealsString += `    </div>
-</div>`
+		`<div id="orderContent%s" class="orderContent"><div class="jidelnicekMain">`,
+		date.Format("2006-01-02"),
+	)
+	// Generate Meal HTML
+	for _, mealItem := range meals {
+		// Begin Item Wrapper
+		mealsString += `<div class="jidelnicekItem " role="group"><div class="jidelnicekItemWrapper">`
+		// Meal Interaction Primary - Order/Cancel
+		state, action := gatherStateAction(mealItem)
+		printer := message.NewPrinter(language.Czech)
+		priceString := printer.Sprintf("%.2f", mealItem.Cena)
+		orderConfirmString := ""
+		if state == "ordered" {
+			orderConfirmString = "Máte objednáno"
+		}
+		mealsString += fmt.Sprintf(
+			`
+			<div class="jidWrapLeft">
+				<a href="#" class="btn button-link button-link-main maxbutton %s"
+					onClick="%s"
+					role="button">
+					<span class="button-link-align">%s<span
+						style="color:black">&nbsp;1&nbsp;ks</span></span>
+					<span class="smallBoldTitle button-link-align">%s</span><span
+						class="button-link-align">za</span>
+					<span class="important warning button-link-align"
+						title="Cena objednaného jídla">%s&nbsp;Kč</span>
+					<span class="button-link-tick">
+						<i class="fa fa-check fa-2x"
+							title="%s<b>&nbsp;1&nbsp;ks</b> á <b>%s&nbsp;Kč</b>"></i>
+					</span>
+				</a>
+			</div>`,
+			// Last Part is diff when not ordered, not just the orderConfirmString, but it hopefully isnt important
+			state, buildMealLink(date, mealItem), action, mealItem.Varianta, priceString, orderConfirmString, priceString,
+		)
+		// Meal Name and Allergens
+		mealsString += fmt.Sprintf(
+			`
+			<div class="jidWrapCenter" id="menu-3-day-%s">
+				%s
+				<span
+				class="textGrey">%s</span>
+				<br>
+			</div>`,
+			date.Format("2006-01-02"), mealItem.Nazev, buildAllergens(mealItem.Alergeny),
+		)
+		// Meal Interaction Secondary - Burza
+		//!! Not Implemented Yet
+		mealsString += fmt.Sprintf(
+			`
+			<div id="icons28428" class="icons jidWrapRight">
+				<i class="far fa-clock fa-2x inlineIcon"
+					title="výdej&nbsp;od:&nbsp;<b>11:00:00</b>&nbsp;do:&nbsp;<b>14:40:00</b><br/> objednat&nbsp;do:&nbsp;<b>20.01.2025 15:00:00</b><br/> zrušit&nbsp;do:&nbsp;<b>20.01.2025 15:00:00</b>"></i>
+			</div>`,
+		)
+		// End Item Wrapper
+		mealsString += `</div></div>`
+	}
+	// End Main Wrapper
+	mealsString += `</div></div>`
+
 	return mealsString
+}
+
+func gatherStateAction(meal Meal) (string, string) {
+	stateString := ""  // ordered | enabled | disabled
+	actionString := "" // (nelze) zrušit | (nelze) objednat | přeobjednat
+	if !meal.LzeObjednat {
+		stateString = "disabled"
+		if meal.Objednano {
+			actionString = "nelze zrušit"
+		} else {
+			actionString = "nelze objednat"
+		}
+	} else {
+		stateString = "enabled"
+		if meal.Objednano {
+			stateString = "ordered"
+			actionString = "zrušit"
+		} else {
+			actionString = "objednat"
+		}
+	}
+	return stateString, actionString
+}
+
+func buildMealLink(date time.Time, meal Meal) string {
+	return `ajaxOrder(this, 'db/dbProcessOrder.jsp?time=1737310888909&amp;token=;ID=755744&amp;day=2025-01-21&amp;type=delete&amp;week=&amp;terminal=false&amp;keyboard=false&amp;printer=false', '2025-01-21', 'ordered')`
+}
+
+func buildAllergens(allergens []Allergen) string {
+	allergensString := `(`
+	for _, allergenItem := range allergens {
+		allergensString += fmt.Sprintf(
+			`<span title="%s" class="textGrey">%s</span><span>, </span>`,
+			allergenItem.Popis, allergenItem.Nazev,
+		)
+	}
+	allergensString += `)`
+	return allergensString
 }
 
 func buildBurza() {
